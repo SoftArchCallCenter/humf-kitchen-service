e97231f691c1a48d005cb058897316ec5cf01f5f
G10N(nick) <nick.supakorn@hotmail.com>
2023-10-16 09:37:09 +0700

    connect gRPC

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
+import { Transport } from "@nestjs/microservices";
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { KITCHEN_PACKAGE_NAME } from '../humf-proto/build/proto/kitchen'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: process.env.URL,
+      package: KITCHEN_PACKAGE_NAME,
+      protoPath: join(__dirname, '../proto/kitchen.proto'),
+    }
+  });
+  app.listen()
 }
 bootstrap();
