42e14bcd74152637e001129755bdb5a8be340f79
bxntn <nuttanan118123@gmail.com>
2023-11-14 19:31:06 +0700

    fix: Change rbmq option

--- src/rmq/rmq.service.ts
+++ src/rmq/rmq.service.ts
@@ -6,7 +6,7 @@ import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
 export class RmqService {
   constructor(private readonly configService: ConfigService) {}
 
-  getOptions(queue: string, noAck = false): RmqOptions {
+  getOptions(queue: string, noAck = true): RmqOptions {
     return {
       transport: Transport.RMQ,
       options: {
@@ -14,6 +14,9 @@ export class RmqService {
         queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
         noAck,
         persistent: true,
+        queueOptions: {
+          durable: true,
+        },
       },
     };
   }
